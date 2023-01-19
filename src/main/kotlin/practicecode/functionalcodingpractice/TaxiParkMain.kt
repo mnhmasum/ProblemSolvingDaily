6285375265232f9bce9633630649d068d746ced7
nazmul <nhasan@ael-bd.com>
2023-01-19 21:54:43 +0600

    finding smart passenger

--- src/main/kotlin/practicecode/functionalcodingpractice/TaxiParkMain.kt
+++ src/main/kotlin/practicecode/functionalcodingpractice/TaxiParkMain.kt
@@ -10,28 +10,41 @@ fun main() {
         setOf(Driver("D1"), Driver("D2")), setOf(Passenger("P1"), Passenger("P2"), Passenger("P3"), Passenger("P4")),
         listOf(
             Trip(Driver("D1"), setOf(Passenger("P1"), Passenger("P2")), 1, 2.5, 3.0),
-            Trip(Driver("D1"), setOf(Passenger("P2"), Passenger("P3")), 1, 2.5, 3.0)
+            Trip(Driver("D1"), setOf(Passenger("P2"), Passenger("P3")), 1, 2.5, null),
+            Trip(Driver("D2"), setOf(Passenger("P2"), Passenger("P3")), 1, 2.5, null),
         )
     )
 
+    //findFaithfulPassenger(taxiPark)
+    //findFrequentPassenger(taxiPark)
+
+    val findSmartPassenger = taxiPark.trips.partition { it.discount == null }
+    println(findSmartPassenger)
+
+}
+
+private fun findFaithfulPassenger(taxiPark: TaxiPark) {
     val r = taxiPark.trips.map { it.passengers }.flatten().groupBy { it.name }
-    val p = taxiPark.allPassengers.asSequence().filter { r.isNotEmpty() }.map { p -> p.name to (r[p.name]?.size ?: 0)
-    }.filter { it.second >= 0 }.map { Passenger(it.first) }.toSet()
+    val findFaithfulPassengers = taxiPark.allPassengers.asSequence()
+        .filter { taxiPark.trips.isNotEmpty() }
+        .map {
+            it.name to (r[it.name]?.size ?: 0)
+        }.filter { it.second >= 0 }.map { Passenger(it.first) }.toSet()
 
-    //println(r)
-    //println(p)
+    println(findFaithfulPassengers)
+}
 
-    val byDriver = taxiPark.trips.filter { it.driver.name == "D1" }
+private fun findFrequentPassenger(taxiPark: TaxiPark) {
+    val findFrequentPassengers = taxiPark.trips.filter { it.driver.name == "D1" }
         .groupBy { it.driver.name }
         .filter { it.key == "D1" }
         .toList()
         .map { it.second }
         .flatten()
         .flatMap { it.passengers }
-        .groupBy { it.name}
+        .groupBy { it.name }
         .filter { it.value.isNotEmpty() }
-        .flatMap { it.value }.
-        distinctBy { it.name }
+        .flatMap { it.value }.distinctBy { it.name }
 
-    println(byDriver)
+    println(findFrequentPassengers)
 }
