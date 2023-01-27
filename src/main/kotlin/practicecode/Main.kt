72e244b80f11e8da04cea9131bfabac4c35d8e8d
nazmul <nhasan@ael-bd.com>
2023-01-27 22:02:36 +0600

    practice rational number coursera projec

--- src/main/kotlin/practicecode/Main.kt
+++ src/main/kotlin/practicecode/Main.kt
@@ -1,95 +1,60 @@
 package practicecode
 
-import java.io.File
+import java.math.BigInteger
 
-class AuthLog(_data: String, _numberOfData: Int) {
 
-    var x: Int = 0
+class Rational(numerator: BigInteger, denometor: BigInteger) {
+    val numer: BigInteger
+    val denom: BigInteger
 
     init {
-        x = _numberOfData
-        //println(_numberOfData)
+        val divisor = numerator.gcd(denometor)
+        val sign = numerator.signum().toBigInteger()
+        numer = numerator / divisor * sign
+        denom = denometor / divisor * sign
     }
 
-    constructor(_data: String, sId: Double) : this(_data + sId, 100) {
-        println(x)
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Rational
+
+        if (numer != other.numer) return false
+        if (denom != other.denom) return false
+
+        return true
     }
 
-    constructor(_numberOfData: Int) : this("From count", 40)
+    override fun hashCode(): Int {
+        var result = numer.hashCode()
+        result = 31 * result + denom.hashCode()
+        return result
+    }
+
 
 }
 
-fun List<Int>.allNonZero() = all { it != 0 }
-fun List<Int>.allNonZero1() = none { it == 0 }
-fun List<Int>.allNonZero2() = any { TODO() }
+infix fun Int.divBy(r2: Int) : Rational = Rational(toBigInteger(), r2.toBigInteger())
 
-fun List<Int>.containsZero(): Boolean = any { it == 0 }
-fun List<Int>.containsZero1() = all { TODO() }
-fun List<Int>.containsZero2() = none { TODO() }
-
-fun isEven(i: Int): Boolean = i % 2 == 0
 
 fun main(args: Array<String>) {
     println("Hello World!")
     val events = listOf(Event("One", true), Event("One", true), Event("Three", true))
+    println(1.plus(4))
+    process("-6".toBigInteger())
+    println(Rational("2".toBigInteger(),"4".toBigInteger()))
+    println(Rational("4".toBigInteger(),"28".toBigInteger()))
+    println("12".toBigInteger().rem("5".toBigInteger()))
 
-    val x = events.groupBy { it.name }
+    val x = 1 divBy 2
+    val y = 4 divBy 28
 
-    //println(x.filter { it.value.size > 1 }.firstNotNullOf { it.value }.toList())
-
-    val t = TaxiPark(
-        setOf(Driver("D1"), Driver("D2")), setOf(Passenger("P1"), Passenger("P2"), Passenger("P3"), Passenger("P4")),
-        listOf(
-            Trip(Driver("D1"), setOf(Passenger("P1"), Passenger("P2")), 1, 2.5, 3.0),
-            Trip(Driver("D1"), setOf(Passenger("P2"), Passenger("P3")), 1, 2.5, 3.0)
-        )
-    )
-
-    val r = t.trips.map { it.passengers }.flatten().groupBy { it.name }
-    val p = t.allPassengers.asSequence().filter { r.isNotEmpty() }.map { p -> p.name to (r[p.name]?.size ?: 0)
-    }.filter { it.second >= 0 }.map { Passenger(it.first) }.toSet()
-
-    //println(r)
-    //println(p)
-
-    val byDriver = t.trips.filter { it.driver.name == "D1" }
-        .groupBy { it.driver.name }
-        .filter { it.key == "D1" }
-        .toList()
-        .map { it.second }
-        .flatten()
-        .flatMap { it.passengers }
-        .groupBy { it.name}
-        .filter { it.value.size > 1 }
-        .flatMap { it.value }.
-        distinctBy { it.name }
-
-
-        //?.groupBy { it.passengers }
-       /* ?.filter { it.value.size>=5 }
-        ?.flatMap { it.value }?.associateBy { it. }?.map { it.value }
-        ?.toSet()*/
-
-
-
-    println(byDriver)
+    println(x == y)
 }
 
-interface OnTest<T> {
-    fun <T> onTestGen(t: T)
-}
-
-fun provideToken(function: (String) -> Unit) {
-    //async call here to an API service
-    function.invoke("mytoken")
-}
-
-fun authenticate(): String? {
-    var token: String? = null
-    provideToken { providedToken ->
-        token = providedToken
-    }
-    return token
+fun process(x: BigInteger) {
+    println(x.signum())
 }
 
 data class Event(var name: String, val isEnable: Boolean)
