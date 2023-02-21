a9cb3420e1ef6865109cb69a63a974d65c2ccb5f
nazmul <nhasan@ael-bd.com>
2023-02-21 21:05:36 +0600

    Maximum Repeating String 1668

--- src/main/kotlin/kotlincode/MaximumRepeatingString1668.kt
+++ src/main/kotlin/kotlincode/MaximumRepeatingString1668.kt
@@ -2,3 +2,18 @@ package kotlincode
 
 class MaximumRepeatingString1668 {
 }
+
+fun main() {
+    var sequence = "aaabaaaabaaabaaaabaaaabaaaabaaaaba"
+    var word = "aaaba"
+    var toBeConcatenated = word
+    var counter = 0
+    while (sequence.contains(word)) {
+        counter++
+        word += toBeConcatenated
+        println(word)
+    }
+
+    println("Result: $counter")
+
+}
