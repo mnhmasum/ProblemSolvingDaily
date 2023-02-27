7e7e299fa09eb2cdfde682438161e910a4e7dc90
nazmul <nhasan@ael-bd.com>
2023-02-27 17:50:51 +0600

    Refactor leetcode problem number 560

--- src/main/kotlin/javacode/SubarraySum560.java
+++ src/main/kotlin/javacode/SubarraySum560.java
@@ -3,8 +3,17 @@ package javacode;
 import java.util.Arrays;
 
 public class SubarraySum560 {
+    public static boolean isPresentInPrefixSumArray(int[] prefixSumArray, int value, int index) {
+        for (int i1 = 0; i1 < index; i1++) {
+            if (value == prefixSumArray[i1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
-        int[] nums = {1,2,3};
+        int[] nums = {1, 2, 3};
         System.out.println(Arrays.toString(nums));
         int k = 3;
         int sumForTarget = 0;
@@ -19,19 +28,14 @@ public class SubarraySum560 {
 
         System.out.println(Arrays.toString(prefixSumArray));
 
-
         for (int i = 0; i < nums.length; i++) {
             sumForTarget = sumForTarget + nums[i];
             if (sumForTarget == k) {
                 subArrayCounter++;
-                //sumForTarget = 0;
-                //continue;
             }
             int value = sumForTarget - k;
-            for (int i1 = 0; i1 < i; i1++) {
-                if (value == prefixSumArray[i1]) {
-                    subArrayCounter++;
-                }
+            if (isPresentInPrefixSumArray(prefixSumArray, value, i)) {
+                subArrayCounter++;
             }
         }
 
