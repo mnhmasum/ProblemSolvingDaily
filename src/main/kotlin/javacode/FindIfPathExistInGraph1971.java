5b19a3802792f919c74010e26485127f169345ae
nazmul <nhasan@ael-bd.com>
2023-03-08 23:52:18 +0600

    Leetcode(1971): Lazy approach by finding root

--- src/main/kotlin/javacode/FindIfPathExistInGraph1971.java
+++ src/main/kotlin/javacode/FindIfPathExistInGraph1971.java
@@ -2,19 +2,13 @@ package javacode;
 
 class FindIfPathExistInGraph1971 {
     public static void union(int p, int q, int[] nums) {
-        //if (nums[p] != nums[q]) {
-        int pid = nums[p];
-        int qid = nums[q];
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == pid) {
-                nums[i] = qid;
-            }
-        }
-        //}
+        int root_p = root(p, nums);
+        int root_q = root(q, nums);
+        nums[root_p] = root_q;
     }
 
     public static boolean isConnected(int p, int q, int[] nums) {
-        return nums[p] == nums[q];
+        return root(p, nums) == root(q, nums);
     }
 
     public static boolean validPath(int n, int[][] edges, int source, int destination) {
@@ -25,22 +19,22 @@ class FindIfPathExistInGraph1971 {
 
         for (int i = 0; i < edges.length; i++) {
             int[] r = edges[i];
-            //System.out.println(Arrays.toString(r)); 
             union(r[0], r[1], nums);
         }
-
-        for (int i = 0; i < n; i++) {
-            System.out.println("val: " + i + " id: " + nums[i]);
-
-        }
-
         return isConnected(source, destination, nums);
 
     }
 
+    public static int root(int q, int[] nums) {
+        while (nums[q] != q) {
+            q = nums[q];
+        }
+        return q;
+    }
+
     public static void main(String[] args) {
-        int[][] x = {{4, 3}, {1, 4}, {4, 8}, {1, 7}, {6, 4}, {4, 2}, {7, 4}, {4, 0}, {0, 9}, {5, 4}};
-        boolean r = validPath(10, x, 5, 9);
+        int[][] x = {{4, 3}, {1, 4}, {1, 7}};
+        boolean r = validPath(10, x, 4, 6);
         System.out.println(r);
     }
 }
