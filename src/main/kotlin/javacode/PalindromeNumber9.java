7288c4e3cf3028217dba6869f93797a4a8f50c69
nazmul <nhasan@ael-bd.com>
2024-05-13 18:25:31 +0600

    PalindromeNumber9

--- src/main/kotlin/javacode/PalindromeNumber9.java
+++ src/main/kotlin/javacode/PalindromeNumber9.java
@@ -3,23 +3,27 @@ package javacode;
 public class PalindromeNumber9 {
     public static void main(String[] args) {
         System.out.println("sdfsfd");
-        int x = 1241;
-        int div = 1;
 
-        while (x >= div * 10) {
-            div = div * 10;
+
+        int x = 1410110141;
+        long div = 1;
+
+
+        while (x > 10 * div) {
+            div = 10 * div;
             System.out.println(div);
         }
 
         while (x != 0) {
-            int left = x / div;
-            int right = x % 10;
+            long left = x / div;
+            long right = x % 10;
             if (left != right) {
                 System.out.println("False");
             }
-            x = (x % div) / 10;
+            x =(int) ((x % div) / 10);
             div = div / 100;
         }
+
 
     }
 }
